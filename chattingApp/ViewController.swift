921a1b856f930dc11a660a929ffbac66ce699c6f
Okan Aslan <okannaslann@hotmail.com>
2023-02-19 11:35:34 +0300

    project started

--- chattingApp/ViewController.swift
+++ chattingApp/ViewController.swift
@@ -8,9 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private lazy var label : UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "project started"
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(label)
+        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         // Do any additional setup after loading the view.
     }
 
